1777906e5a8970092d61cb863483724f2dbd448c
Triparna Kar <kartriparna53@gmail.com>
2025-07-23 11:51:33 +0530

    intial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-9xl font-bold flex items-center justify-center h-full">CodeRoom</h1>
+    </div>
+  );
+}
